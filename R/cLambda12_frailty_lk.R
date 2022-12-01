44eb3dc006e2bff582cbfb1842773cf9166cc4b9
Emily Roberts <ekrobe@umich.edu>
2022-11-30 21:20:28 -0600

    update example

--- R/cLambda12_frailty_lk.R
+++ R/cLambda12_frailty_lk.R
@@ -6,8 +6,8 @@
 #'
 #' @return likelihood
 #'
-#' @examples likelihood for given parameter set
-#' 
-cLambda12_frailty_lk<-function(x,xdata1, omega1, shape12, scale12, c12, beta12_1) {
- return( -pweibull(x, scale=1/scale12, shape= shape12, lower = F, log = T)*exp(beta12_1*xdata1 + c12*omega1))
+#' @examples
+#' cLambda12_frailty_lk(x = 1, xdata1 = 0, omega1 = 1, shape12 = 1, scale12 = 1, c12 = 1, beta12_1 = 0)
+cLambda12_frailty_lk<-function(x, xdata1, omega1, shape12, scale12, c12, beta12_1) {
+ return(-pweibull(x, scale = 1/scale12, shape = shape12, lower = F, log = T) * exp(beta12_1 * xdata1 + c12 * omega1))
   }
